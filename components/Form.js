4c50fce9457f8a641bae59fec3c442d1e82cf5c7
hellcattc <slhtte345@gmail.com>
2024-01-13 17:09:31 +0300

    Refactor input

--- components/Form.js
+++ components/Form.js
@@ -2,24 +2,22 @@ import { useCallback, useRef } from "react";
 import { Button, TextInput, View } from "react-native";
 
 const Form = ({ handlePress }) => {
-  const taskInput = useRef(null);
-  const handleInput = useCallback(
-    (text) => {
-      taskInput.current = text;
-    },
-    [taskInput]
-  );
+  const inputRef = useRef(null);
 
   return (
     <View className="pt-10 w-full">
       <TextInput
-        onChangeText={handleInput}
+        ref={inputRef}
+        onChangeText={(text) => (inputRef.current.value = text)}
         placeholder="Текст задачи..."
         className="border rounded-sm border-black px-3 py-2 mb-4"
       ></TextInput>
       <Button
         title="Добавить задачу"
-        onPress={() => handlePress(taskInput.current)}
+        onPress={() => {
+          inputRef.current.clear();
+          handlePress(inputRef.current.value);
+        }}
         color="#8c4a0b"
       ></Button>
     </View>
